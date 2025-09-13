ce13b300a6dbc0a3294c6e105728dca274bebbf9
tmuraina <133337460+tmuraina@users.noreply.github.com>
2025-09-13 17:29:56 +0100

    listings path corrected

--- alx_travel_app/listings/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Listing
-
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Listing
-        fields = ['id', 'title', 'description', 'location', 'price_per_night', 'created_at', 'updated_at', 'host']
-        read_only_fields = ['id', 'created_at', 'updated_at']
